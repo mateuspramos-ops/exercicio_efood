6cd2b32b1f85e3905307fa15786ba46739836d14
mateuspramos-ops <mateusp.ramos@gmail.com>
2026-06-22 13:33:55 -0300

    Add files via upload

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import RestauranteCard from '../../components/RestauranteCard'
+import restaurantes from '../../data/restaurantes'
+import { cores } from '../../styles/GlobalStyle'
+
+const Main = styled.main`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 80px 120px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 48px 80px;
+`
+
+const Home = () => {
+  return (
+    <>
+      <Header variant="home" />
+      <Main>
+        <Grid>
+          {restaurantes.map((r) => (
+            <RestauranteCard key={r.id} restaurante={r} />
+          ))}
+        </Grid>
+      </Main>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
